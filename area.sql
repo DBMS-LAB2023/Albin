4e0b602b4aeae5722aa6c3ac03563a04c82fdaeb
Albin xavier <118426976+xavier23231@users.noreply.github.com>
2023-08-28 22:05:57 -0700

    Add files via upload

---
+++ area.sql
@@ -0,0 +1,11 @@
+create or replace function area(n1 in number,n2 in number)
+return number
+is
+n3 number(8);
+begin
+n3:=n1*n2;
+return n3;
+end;
+/
+ 7 changes: 7 additions & 0 deletions7  
+main.sql
---
+++ test3.sql
@@ -0,0 +1,5 @@
+begin
+for no in 1..5 loop
+dbms_output.put_line('Iteration:'||no);
+end loop;
+end;
